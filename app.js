b4660807860bcb5fbd100e23b7fd77499eaa9f27
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-08-15 14:37:34 +0200

    New file structure, breaking the code up in modules

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 // README - Middleware ~ is a function that can modify the incoming request data 
@@ -19,177 +21,9 @@ app.use((req, res, next) => {
 });
 
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-
-// README - ROUTE HANDLERS 
-
-const getAllTours = (req, res) => {
-	console.log(req.requestTime);
-	res.status(200).json({
-		status: 'success',
-		requestedAt: req.requestTime,
-		results: tours.length,
-		data: {
-			tours
-		}
-	});
-};
-
-const getTour =  (req, res) => {
-	console.log(req.params);
-
-	// Covert string into number
-	const id = req.params.id * 1;
-	const tour = tours.find(el => el.id === id);
-
-	// if (id > tour.length)
-	if(!tour) {
-		return res.status(404).json({
-			status: 'fail',
-			message: 'invalid ID'
-		})
-	}
-
-	res.status(200).json({
-		status: 'success',
-		data: {
-			tour
-		}
-	});
-};
-
-const createTour = (req, res) => {
-	// console.log(req.body);
-
-	const newId = tours[tours.length - 1].id + 1;
-	const newTour = Object.assign({id : newId}, req.body);
-
-	tours.push(newTour);
-
-	fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
-			res.status(201).json({
-				status: 'success',
-				data: {
-					tours: newTour
-				}
-			});
-	    }
-    );
-};
-
-const updateTour = (req, res) => {
-	 if (req.params.id * 1 > tours.length) {
-		return res.status(404).json({
-			status: 'fail',
-			message: 'invalid ID'
-		});
-	}
-
-	res.status(200).json({
-		status: 'success',
-		data: {
-			tour: '<Updated tour here...>'
-		}
-	});
-};
-
-const deleteTour = (req, res) => {
-	 if (req.params.id * 1 > tours.length) {
-		return res.status(404).json({
-			status: 'fail',
-			message: 'invalid ID'
-		});
-	}
-
-	res.status(204).json({
-		status: 'success',
-		data: null
-	});
-};
-
-const getAllUsers = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined'
-	});
-};
-
-const getUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined'
-	});
-};
-
-const createUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined'
-	});
-};
-
-const updateUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined'
-	});
-};
-
-const deleteUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined'
-	});
-};
-
-// README - HTTP REQUEST METHODS 
-
-/*
-app.get('/api/v1/tours', getAllTours);
-
-app.get('/api/v1/tours/:id', getTour);
-
-app.post('/api/v1/tours', createTour);
-
-app.patch('/api/v1/tours/:id', updateTour);
-
-app.delete('/api/v1/tours/:id', deleteTour);
-*/
-
-// README CREATING MULTIPLE ROUTERS
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-// README TOUR HANDLERS
-
-tourRouter.route('/')
-	.get(getAllTours)
-	.post(createTour);
-
-tourRouter.route('/:id')
-	.get(getTour)
-	.patch(updateTour)
-	.delete(deleteTour);
-
-// README USER HANDLERS
-
-userRouter.route('/')
-	.get(getAllUsers)
-	.post(createUser);
-
-userRouter.route('/:id')
-	.get(getUser)
-	.patch(updateUser)
-	.delete(deleteUser);
-
 // README MOUNTING THE ROUTERS
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-
-
-const port = 3000;
-app.listen(port, () => {
-	console.log('App runing on port 3000');
-});
+module.exports = app;
 
---
+++ routes/tourRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+// const tourController = require('./../controllers/tourController');
+
+const { getAllTours, 
+		createTour, 
+		getTour, 
+		updateTour, 
+		deleteTour } = require('./../controllers/tourController');
+
+// README TOUR HANDLERS
+
+const router = express.Router();
+
+router.route('/')
+	.get(getAllTours)
+	.post(createTour);
+
+router.route('/:id')
+	.get(getTour)
+	.patch(updateTour)
+	.delete();
+
+	module.exports = router;
