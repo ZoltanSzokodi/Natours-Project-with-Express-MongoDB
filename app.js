d004fd68d059a5906a09c1c946cc82288f76ac83
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-08-15 11:46:59 +0200

    Practicing with custom middleware

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-
-// Middleware ~ is a function that can modify the incoming request data README
+// README - Middleware ~ is a function that can modify the incoming request data 
 app.use(express.json());
 
-/*
-app.get('/', (req, res) => {
-	res
-	.status(200)
-	.json({message: 'Hello from the server side!', app: 'Natours'});
+app.use((req, res, next) => {
+	console.log('Hello from the middleware');
+	next();
 });
 
-app.post('/', (req, res) => {
-	res.send('You can post to this endpoint...');
-})
-*/
+app.use((req, res, next) => {
+	req.requestTime = new Date().toISOString();
+	next();
+});
 
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-// Get all tours README
-app.get('/api/v1/tours', (req, res) => {
+// README - CALLBACKS 
+
+const getAllTours = (req, res) => {
+	console.log(req.requestTime);
 	res.status(200).json({
 		status: 'success',
+		requestedAt: req.requestTime,
 		results: tours.length,
 		data: {
 			tours
 		}
 	});
-});
+};
 
-// Get single tour README
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour =  (req, res) => {
 	console.log(req.params);
 
 	// Covert string into number
@@ -53,11 +52,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
 			tour
 		}
 	});
-});
+};
 
-
-// Post to tours README
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
 	// console.log(req.body);
 
 	const newId = tours[tours.length - 1].id + 1;
@@ -74,10 +71,9 @@ app.post('/api/v1/tours', (req, res) => {
 			});
 	    }
     );
-});
+};
 
-// Patch README
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
 	 if (req.params.id * 1 > tours.length) {
 		return res.status(404).json({
 			status: 'fail',
@@ -91,10 +87,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
 			tour: '<Updated tour here...>'
 		}
 	});
-});
+};
 
-// Delete README
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
 	 if (req.params.id * 1 > tours.length) {
 		return res.status(404).json({
 			status: 'fail',
@@ -106,8 +101,30 @@ app.delete('/api/v1/tours/:id', (req, res) => {
 		status: 'success',
 		data: null
 	});
-});
+};
 
+// README - HTTP REQUEST METHODS 
+
+/*
+app.get('/api/v1/tours', getAllTours);
+
+app.get('/api/v1/tours/:id', getTour);
+
+app.post('/api/v1/tours', createTour);
+
+app.patch('/api/v1/tours/:id', updateTour);
+
+app.delete('/api/v1/tours/:id', deleteTour);
+*/
+
+app.route('/api/v1/tours')
+	.get(getAllTours)
+	.post(createTour);
+
+app.route('/api/v1/tours/:id')
+	.get(getTour)
+	.patch(updateTour)
+	.delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
