0623f50273666ee9fb0352dd3e2842d56ed0c369
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-08-22 18:34:41 +0200

    Improving the API -> filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,8 +5,32 @@ const Tour = require('./../models/tourModel');
 // README - ROUTE HANDLERS 
 exports.getAllTours = async (req, res) => {
 	try {
-		const tours = await Tour.find();
+		console.log(req.query);
 
+		// README BUILD QUERY
+		// 1) Filtering
+		const queryObj = { ...req.query };
+		const excludedFields = ['page', 'sort', 'limit', 'fields'];
+		excludedFields.forEach(el => delete queryObj[el]);
+
+		// 2) Advanced filtering
+		let queryStr = JSON.stringify(queryObj);
+		queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+		
+		console.log(JSON.parse(queryStr));
+
+		const query = Tour.find(JSON.parse(queryStr));
+
+		// README EXECUTE QUERY
+		const tours = await query;
+
+		// const query = Tour.find()
+		// 	.where('duration')
+		// 	.equals(5)
+		// 	.where('difficulty')
+		// 	.equals('easy');
+
+		// README SEND RESPONSE
 		res.status(200).json({
 			status: 'success',
 			results: tours.length,
