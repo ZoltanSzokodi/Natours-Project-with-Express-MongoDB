10d566775572b1f0a1f18e5fe78ddfed7f42a2f1
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-08-24 15:14:13 +0200

    Document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const slugify = require('slugify');
 const toursSchema = new mongoose.Schema({
 	name: {
 		type: String,
@@ -7,6 +7,7 @@ const toursSchema = new mongoose.Schema({
 		unique: true,
 		trim: true
 	},
+	slug: String,
 	duration: {
 		type: Number,
 		required: [true, 'A tour must have a duration']
@@ -60,6 +61,12 @@ const toursSchema = new mongoose.Schema({
 // Virtual property - this is actually not part of the DB
 toursSchema.virtual('durationWeeks').get(function() {
 	return this.duration / 7;
+});
+
+// README DOCUMENT MIDDLEWARE - runs before .save() and .create()
+toursSchema.pre('save', function(next) {
+	this.slug = slugify(this.name, { lower: true});
+	next();
 });
 
 const Tour = mongoose.model('Tour', toursSchema);
