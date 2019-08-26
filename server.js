28723ed07833aaa3f56b1d3d74fde01b7ab236a1
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-08-26 11:34:50 +0200

    Error outside Express -> Unhandled Rejection, Shutting down process

--- server.js
+++ server.js
@@ -11,14 +11,22 @@ mongoose.connect(DB, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useFindAndModify: false
-}).then(con => {
+}).then(() => {
 	// console.log(con.connections);
 	console.log('DB connection successful!');
-}).catch(err => console.log(err.message));
+});
 
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+	console.log(err.name, err.message);
+	console.log('UNHANDLED REJECTION! Shutting down...');
+	server.close(() => {
+		process.exit(1);
+	})
 });
