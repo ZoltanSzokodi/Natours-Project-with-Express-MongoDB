e22fe816adceedcaaf7b6952893b043016fb05de
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-08-26 11:16:42 +0200

    Handling Mongoose Validation Errors

--- controllers/errorContoller.js
+++ controllers/errorContoller.js
@@ -1,17 +1,24 @@
-const AppError = require('./../utils/appError');
+const AppError = require("./../utils/appError");
 
 const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
-}
+};
 
 const handleDuplicateFieldsDB = err => {
-  const value = err.errmsg.match(/([\"'])(?:\\\1|.)*?\1/)[0];
+  const value = err.errmsg.match(/(["'])(?:\\\1|.)*?\1/)[0];
 
   const message = `Duplicate field value: ${value}. Please use another value!`;
 
   return new AppError(message, 400);
-}
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -20,7 +27,7 @@ const sendErrorDev = (err, res) => {
     message: err.message,
     stack: err.stack
   });
-}
+};
 
 const sendErrorProd = (err, res) => {
   // Operational, trusted error: send message to client
@@ -33,30 +40,32 @@ const sendErrorProd = (err, res) => {
     // Programing or other unknown error: dont't leak error details
   } else {
     // 1) Log error
-    console.error('Error', err);
+    console.error("Error", err);
 
     // 2) Send generic message
     res.status(500).json({
-      status: 'error',
-      message: 'Something went wrong'
+      status: "error",
+      message: "Something went wrong"
     });
   }
 };
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+  err.status = err.status || "error";
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'production') {
+  } else if (process.env.NODE_ENV === "production") {
     let error = {
       ...err
     };
 
-    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.name === "CastError") error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
-}
+};
