8aa89f65add951e40fde64f07806d55a72da3757
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-08-29 12:27:04 +0200

    Setting Security HTTP Headers -> helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorContoller');
@@ -13,6 +14,10 @@ const app = express();
 // console.log(process.env.NODE_ENV);
 
 // 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 };
@@ -24,9 +29,15 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from the body into req.body
+app.use(express.json({
+	limit: '10kb'
+}));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
 	req.requestTime = new Date().toISOString();
 	// console.log(req.headers);
