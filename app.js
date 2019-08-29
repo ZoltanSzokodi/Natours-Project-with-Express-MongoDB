ea243cfe6f9f2d774a2015ad21743582201ba530
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-08-29 12:15:54 +0200

    Implementing rate limit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorContoller');
@@ -10,9 +11,18 @@ const app = express();
 
 // README - Middleware ~ is a function that can modify the incoming request data 
 // console.log(process.env.NODE_ENV);
+
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 };
+
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many requests from this IP, please try again in an hour'
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
