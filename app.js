53bdf0e9f56429be75c81af78947c1bc9553d8ec
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-09-03 13:44:07 +0200

    Creating Our Base Pug Template

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,10 +15,16 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // README - Middleware ~ is a function that can modify the incoming request data 
 // console.log(process.env.NODE_ENV);
 
-// 1) GLOBAL MIDDLEWARES
+// GLOBAL MIDDLEWARES
 // Set security HTTP headers
 app.use(helmet());
 
@@ -57,9 +64,6 @@ app.use(
 		]
 	}));
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test middleware
 app.use((req, res, next) => {
 	req.requestTime = new Date().toISOString();
@@ -68,6 +72,13 @@ app.use((req, res, next) => {
 });
 
 // README MOUNTING THE ROUTERS
+app.get('/', (req, res) => {
+	res.status(200).render('base', {
+		tour: 'The Forest Hiker',
+		user: 'Jonas'
+	});
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
