7ac3514c34243293ee1f908bd875365a603724fb
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-09-09 10:39:45 +0200

    Render login page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -10,7 +10,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({
     slug: req.params.slug
   }).populate({
@@ -23,3 +23,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
